f2061c8a4b6aa468afc22abd82e6945448b4e6b9
Yan1702 <yzha666888@gmail.com>
2025-08-03 15:20:37 +1000

    update app.py

--- CapstoneProject/app.py
+++ CapstoneProject/app.py
@@ -20,9 +20,8 @@ class_names = [
 
 # Load Image Modle
 def load_image_model():
-    #model = load_model('CapstoneProject/best_model.keras')
-    model = load_model('CapstoneProject/best_model.keras', custom_objects={})
-    print(model.input_shape)  # Check the expected input shape
+    model = load_model('CapstoneProject/best_model.keras')
+    #model = load_model('CapstoneProject/best_model.keras', custom_objects={})
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
